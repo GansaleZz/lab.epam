4a9dfa507f530cc91276bed57943ddf193a52482
GansaleZz <72818524+GansaleZz@users.noreply.github.com>
2021-09-30 23:06:04 +0300

    Update Tag.java

--- src/main/java/com/epam/esm/persistence/entity/Tag.java
+++ src/main/java/com/epam/esm/persistence/entity/Tag.java
@@ -8,9 +8,9 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.GenerationType;
 
 @Entity
 @Table(name = "tag")
